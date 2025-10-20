9e0b85a3c9d9069c12b4fe715ea9adfe395e5dd3
kdowd <kevin.dowd@gmail.com>
2025-10-20 17:37:45 +1300

    working with LINQ

--- Views/Components/Login.xaml.cs
+++ Views/Components/Login.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WPFLoginJoin.Database;
+using WPFLoginJoin.ViewModels;
 
 namespace WPFLoginJoin.Views.Components
 {
@@ -21,6 +25,9 @@ namespace WPFLoginJoin.Views.Components
     /// </summary>
     public partial class Login : UserControl
     {
+        public bool loggedStatus { get; set; } = false;
+
+
         public Login()
         {
             InitializeComponent();
@@ -29,6 +36,53 @@ namespace WPFLoginJoin.Views.Components
 
         private void OnCheckLogin(object sender, RoutedEventArgs e)
         {
+
+            IMongoDatabase? theDatabase = Connection.database;
+
+            if (theDatabase == null)
+            {
+                return;
+            }
+
+            //username "admin1"
+            //password "password123"
+            IQueryable<AdminsDTO> theCollection = theDatabase.GetCollection<AdminsDTO>("admins").AsQueryable<AdminsDTO>();
+            List<AdminsDTO> theCollectionAsList = theCollection.ToList<AdminsDTO>();
+            string userPass = UserPassword.Text.Trim();
+            string userName = UserName.Text.Trim();
+
+            if (!String.IsNullOrEmpty(userPass) && !String.IsNullOrEmpty(userName))
+            {
+                bool test = theCollectionAsList.Any(
+                   (item) => { return item.Password.Equals(userPass) && item.Username.Equals(userName); }
+                   );
+
+                MessageBox.Show(test.ToString());
+            }
+
+
+            //theCollectionAsList.ForEach(a =>
+            //{
+
+            //    if (a.Password == "password123")
+            //    {
+            //        loggedStatus = true;
+            //        MessageBox.Show("PASS");
+
+            //    }
+            //});
+
+
+
+            //var index = theCollection.Select((value, index) => new { value, index });
+
+            // int indexOfKiwi = theCollection.Select((value, index) => new { value, index })
+            //                    .Where(item => item.user == "Kiwi")
+            //                    .Select(item => item.index)
+            //                    .DefaultIfEmpty(-1) // If no match, return -1
+            //                    .FirstOrDefault();
+
+
 
         }
     }
